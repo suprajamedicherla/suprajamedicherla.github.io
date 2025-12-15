e428a4ac9d4745f8f492f03047c652004d8f84fa
suprajamedicherla <143642452+suprajamedicherla@users.noreply.github.com>
2025-12-15 13:47:05 -0600

    Add event listener for portfolio load success

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const text = "Health Informatics • Machine Learning • Health Data Science";
-let index = 0;
-
-function typeWriter() {
-  if (index < text.length) {
-    document.getElementById("typing").innerHTML += text.charAt(index);
-    index++;
-    setTimeout(typeWriter, 70);
-  }
-}
-
-typeWriter();
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Portfolio Loaded Successfully");
+});
